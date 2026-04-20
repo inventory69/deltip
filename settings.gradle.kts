8ad1220eccceb305ac45fa58bf3c3bff51aec399
inventory69 <admin@sad.run>
2026-04-20 10:00:00 +0200

    chore: Projekt mit Gradle 9.4.1, Kotlin 2.2.21 und Compose Multiplatform 1.10.3 aufsetzen
    Wrapper, Version-Catalog und Root-Build-Skripte fuer ein KMP-Projekt mit JDK 21 und Android-Compile-SDK 36.

---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        // maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")  // optional, falls neuere CMP nötig
+    }
+}
+rootProject.name = "Deltip"
+include(":composeApp")
